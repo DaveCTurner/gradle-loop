2e090d3ec7de6adfd77098786be31976b3c76068
David Turner <david.turner@elastic.co>
2018-09-29 09:31:59 +0100

    Run gradle in a loop

--- Main.hs
+++ Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import System.Environment
+import Data.Conduit
+import Data.Conduit.Process
+import Data.Conduit.Combinators
+import Control.Monad.Trans.Resource
+import System.Exit
+
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  putStrLn $ "starting with " ++ show args
+  loop args
+
+loop :: [String] -> IO ()
+loop args = do
+  (exitCode, (), ()) <- runResourceT $
+    sourceProcessWithStreams
+      (proc "./gradlew" args)
+        { new_session   = True
+        , delegate_ctlc = True
+        }
+      (return ()) -- stdin
+      (sinkFile "testoutput-stdout.log")
+      (sinkFile "testoutput-stderr.log")
+
+  putStrLn $ "finished with " ++ show exitCode
+  case exitCode of
+    ExitSuccess -> loop args
+    _           -> return ()
